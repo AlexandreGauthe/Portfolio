3ece35252029afdd5ac1efde4e974eea66b1aa55
AlexandreGauthe <155651944+AlexandreGauthe@users.noreply.github.com>
2024-06-06 10:34:34 +0200

    initialisation du projet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import Home from'./pages/Home';
+import Projet from './pages/Projet';
+import Error from './pages/Error';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='./pages/Projet' element={<Projet/>} />
+          <Route path='*' element={<Error />} /> 
+        </Routes>
+    </Router>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Projet/index.jsx
@@ -0,0 +1,5 @@
+export default function Projet(){
+    return (
+        <h1> Test </h1>
+    )
+}
