a89fa378f7d3b3a702b31a2a5282e4b4077adc3d
AlexandreGauthe <155651944+AlexandreGauthe@users.noreply.github.com>
2024-06-13 17:23:52 +0200

    Début implémentation du formulaire de contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,51 @@
 // Composant formulaire de contact //
 import React from "react";
+import emailjs from '@emailjs/browser';
 import "../styles/Contact.scss";
+import {useRef} from 'react';
 
-export default function Contact(){
+export const Contact = () => {
+	const form = useRef();
+
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_rpo3a7l", "template_fem8t6w", form.current, {
+        publicKey: "ANms_DPE-cDgWZ9SB",
+      })
+      .then(
+        (result) => {
+		  console.log(result.text);
+          console.log("Mail envoyé");
+        },
+        (error) => {
+		  console.log(error.text);	
+        },
+      );
+  };
     return(
         <div id="contact">
 	        <div className="container">
-		        <div className="form-container">
-			        <h2>Une question ? Une demande de devis ?</h2>
-			        <p> N’hésitez pas à m’écrire via le formulaire ci-dessous :</p>
-			        <form action="#" method="post">
-				        <label for="nom" aria-label="Entrez votre nom">Nom</label>
-				        <input type="text" name="nom" id="nom"/>
-                        <label for="prenom" aria-label="Entrez votre prénom">Nom</label>
-				        <input type="text" name="prenom" id="prenom"/>
-				        <label for="email" aria-label="Entrez votre adresse mail">Email</label>
+		        <h2 className="form_title">Une question ou une demande de devis ?</h2>
+			    <p className="form_text"> N’hésitez pas à m’écrire via le formulaire ci-dessous :</p>
+				<div className="form-container">
+			        <form  ref={form} onSubmit={sendEmail}>
+				        <label aria-label="Entrez votre nom">Nom</label>
+				        <input type="text" name="user_name" id="user_name"/>
+                        <label aria-label="Entrez votre prénom">Prénom</label>
+				        <input type="text" name="user_first_name" id="user_first_name"/>
+						<label aria-label="Entrez votre numéro de téléphone">Téléphone</label>
+						<input type="tel" name="phone" id="phone"/>
+				        <label aria-label="Entrez votre adresse mail">Email</label>
 				        <input type="email" name="email" id="email"/>
-				        <label for="message" aria-label="Entrez votre message">Message</label>
+				        <label aria-label="Entrez votre message">Message</label>
 				        <textarea name="message" id="message" cols="30" rows="10"></textarea>
-				        <input type="submit" aria-label="Cliquez pour envoyer" value="Envoyer"/>
-			        </form>
+				        <input className="button" type="submit" aria-label="Cliquez pour envoyer" value="Envoyer"/>
+					</form>
 		        </div>
 	        </div>
         </div>
     )
 }
+
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header";
 import Intro from "../../components/Intro";
 import About from "../../components/About";
 import Projet from "../../components/Projet";
-import Contact from "../../components/Contact";
+import {Contact} from "../../components/Contact"
 import Footer from "../../components/Footer";
 export default function Home() {
   return (
