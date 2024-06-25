da1b1c80e9e04492e6efbb2e8eb6a191eea4c84e
AlexandreGauthe <155651944+AlexandreGauthe@users.noreply.github.com>
2024-06-25 13:47:00 +0200

    Site responsive pas encore publié

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ export default function Footer(){
        <div className="footer-block"> 
             <h2>Mes réseaux sociaux :</h2>
             <div className="link-block">
-              <a href="https://www.linkedin.com/in/alexandre-gauthe-ab454011a/" target="blank" rel="noopener noreferrer"><img src="https://onedrive.live.com/embed?resid=91F849ED5F05CE92%2121705&authkey=%21AJihHSr15tZ4QN4&width=300&height=300" alt="Icone de Linkedind" title="Linkedin"></img></a>
-              <a href="https://viadeo.journaldunet.com/p/alexandre-gauthe-2582042" target="blank" rel="noopener noreferrer"><img src="https://onedrive.live.com/embed?resid=91F849ED5F05CE92%2121707&authkey=%21ABpWoiaVo69DL2w&width=300&height=298" alt="Logo de Viadeo" title="Viadeo"></img></a>  
-              <a href="https://github.com/AlexandreGauthe" target="blank" rel="noopener noreferrer" ><img src="https://onedrive.live.com/embed?resid=91F849ED5F05CE92%2121706&authkey=%21AEZeTRrLxfP9obY&width=300&height=300" alt="Logo de Github" title="Github"></img></a>  
+              <a href="https://www.linkedin.com/in/alexandre-gauthe-ab454011a/" target="blank" rel="noopener noreferrer"><img src="https://res.cloudinary.com/diy32kbiz/image/upload/v1718962141/Linkedin-logo-on-transparent-Background-PNG-_um9dbm.png" alt="Icone de Linkedind" title="Linkedin"></img></a>
+              <a href="https://viadeo.journaldunet.com/p/alexandre-gauthe-2582042" target="blank" rel="noopener noreferrer"><img src="https://res.cloudinary.com/diy32kbiz/image/upload/v1718962142/Logo_Viadeo_vfg45o.png" alt="Logo de Viadeo" title="Viadeo"></img></a>  
+              <a href="https://github.com/AlexandreGauthe" target="blank" rel="noopener noreferrer" ><img src="https://res.cloudinary.com/diy32kbiz/image/upload/v1718962140/Github_x22u7s.png" alt="Logo de Github" title="Github"></img></a>  
             </div>
         </div>    
     )
--- src/pages/Projet/index.jsx
+++ src/pages/Projet/index.jsx
@@ -26,10 +26,11 @@ export default function Projet(){
     
     if(!selectedProject) {return null;};
 
-    const {title,description,pictures,technologies,link,points,diffculties,solutions} = selectedProject;
-    const list_points = points.map((point) => <li>{point}</li>);
-    const list_diffculties = diffculties.map((diffculty) => <li>{diffculty}</li>);
-    const list_solutions = solutions.map((solution) => <li>{solution}</li>);
+    const {title,description,pictures,technologies,link,points,diffculties,solutions,alt} = selectedProject;
+    const points_list = points.map((point) => <li>{point}</li>);
+    const diffculties_list = diffculties.map((diffculty) => <li>{diffculty}</li>);
+    const solutions_list = solutions.map((solution) => <li>{solution}</li>);
+    const technologies_list = technologies.map((technologie) => <img src={technologie} alt={alt[selectedPicture]} title={alt[selectedPicture]}/>)
     
     const nextPictOnClick = (event) =>{
         selectedPicture <= pictures.length -2 ? setSelectedPicture(selectedPicture + 1) : setSelectedPicture(0);
@@ -41,27 +42,33 @@ export default function Projet(){
         <div>
            <Header style={{position:"static"}}/>
            <div className="main_box">
-                <h2>{title} :</h2>
+                <h2 id="title">{title} :</h2>
                 <div className="slider_box">
-                    <Slider src={pictures[selectedPicture]} alt={title} count= {pictures.length === 1 ? null :`${selectedPicture +1} / ${pictures.length}`} nextOnClick={nextPictOnClick} style={pictures.length===1 ?{display:"none"} : null} prevOnClick={prevPictOnclick}/>
+                    <Slider src={pictures[selectedPicture]} alt={title} count={pictures.length === 1 ? null :`${selectedPicture +1} / ${pictures.length}`} nextOnClick={nextPictOnClick} style={pictures.length===1 ?{display:"none"} : null} prevOnClick={prevPictOnclick}/>
                 </div>
             </div>    
             <p className="description">{description}</p>
             <div className="project_text_block">
-                <h2 className="project_subtitle">Caractéristiques du projet :</h2>
+                <h2 className="project_subtitle">Caractéristiques du projet</h2>
                 <div className="project_boards">
                     <div className="main_points">
                         <h3>Points principaux du projet :</h3>
-                        <ul>{list_points}</ul>
+                        <ul>{points_list}</ul>
                     </div>
                     <div className="difficulties">
                         <h3>Difficultés du projet :</h3>
-                        <ul>{list_diffculties}</ul>
+                        <ul>{diffculties_list}</ul>
                     </div>
                     <div className="solutions">
                         <h3>Solutions employées :</h3>
-                        <ul>{list_solutions}</ul>
+                        <ul>{solutions_list}</ul>
                     </div>
+                </div>
+                <div className="technos">
+                    <h3>Technologies utilisées :</h3>
+                    <div className="technos__logos">
+                        {technologies_list}
+                    </div>   
                 </div>
                 <div className="project-link">
                     <a id="github_link" href={link} target="blank">Lien vers le projet</a>  
