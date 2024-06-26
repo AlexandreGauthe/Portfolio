e21dfe1eda818d20b1f68ee078016449b5f4d602
AlexandreGauthe <155651944+AlexandreGauthe@users.noreply.github.com>
2024-06-26 09:31:53 +0200

    deploiement application

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
       <Routes>
         <Route exact path='/Portfolio' element={<Home />}/>
         <Route path='/projet/:projetid' element={<Projet />} />
-        <Route path='*/' element={<Error />} /> 
+        <Route path='/*' element={<Error />} /> 
       </Routes>
     </Router>
     </React.StrictMode>,
--- src/pages/Projet/index.jsx
+++ src/pages/Projet/index.jsx
@@ -19,7 +19,7 @@ export default function Projet(){
     
     useEffect(() =>{ 
         if (!selectedProject){
-            navigate("Portfolio/error");
+            navigate("/Portfolio/error");
         }else {
             document.title = `${selectedProject.title}`;
             }},[selectedProject,navigate()]);
